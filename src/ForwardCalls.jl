1575721d65656c89d65ea91f867f3cfd8818fbc2
Giorgio Calderone <giorgio.calderone@gmail.com>
2019-01-09 19:49:25 +0100

    Small fix

--- src/ForwardCalls.jl
+++ src/ForwardCalls.jl
@@ -1,6 +1,8 @@
 module ForwardCalls
 using InteractiveUtils
-export supertypes, forward, @forward, @inherit_fields, concretetype, isinheritable, @inheritable
+
+export supertypes, forward, @forward,
+    @inherit_fields, concretetype, isinheritable, @inheritable
 
 """
 supertypes(T::Type)
@@ -21,32 +23,30 @@ end
 
 
 
-# function forward(send::Tuple{Type,Symbol}, recvs::Vector{<:Type}; kw...)
-#     out = Vector{String}()
-#     for recv in recvs
-#         append!(out, forward(send, recv; kw...))
-#     end
-#     return unique(out)
-# end
+"""
 
-function forward(send::Tuple{Type,Symbol}, recv::T; super=true, kw...) where T <: Type
-    tt = [recv]
-    (super)  &&  (append!(tt, supertypes(recv)))
-    return forward(send, tt, methodswith(recv, supertypes=super); kw...)
+```
+struct Wrapper{T}
+    wrappee::T
+    extra
+    Wrapper{T}(args...; kw...) where T = new(T(args...; kw...), nothing)
 end
+eval.(Meta.parse.(forward((Wrapper, :wrappee), Int, which(+, (Int, Int)))))
 
-forward(send::Tuple{Type,Symbol}, recvs::Vector{<:Type}, method::Method; kw...) = forward(send, recvs, [method]; kw...)
-forward(send::Tuple{Type,Symbol}, recv::Type, methods::Vector{Method}; kw...) = forward(send, [recv], methods; kw...)
-
-function forward(send::Tuple{Type,Symbol}, recvs::Vector{T}, methods::Vector{Method};
-                 usetypes=true, useallargs=true) where T <: Type
-    function fwd_method_as_string(send_type, send_symb, argid, method, usetypes, useallargs)
-        #pf(m, s) = @eval(m, parentmodule($s))
+i1 = Wrapper{Int}(1)
+i2 = Wrapper{Int}(2)
+println(i1 +  2)
+println( 1 + i2)
+println(i1 + i2)
+```
+"""
+function forward(sender::Tuple{Type,Symbol}, receiver::Type, method::Method;
+                 withtypes=true, allargs=true)
+    function newmethod(sender_type, sender_symb, argid, method, withtypes, allargs)
         s = "p" .* string.(1:method.nargs-1)
-        (usetypes)  &&
-            (s .*= "::" .* string.(fieldtype.(Ref(method.sig), 2:method.nargs)))
-        s[argid] .= "p" .* string.(argid) .* "::$send_type"
-        if !useallargs
+        (withtypes)  &&  (s .*= "::" .* string.(fieldtype.(Ref(method.sig), 2:method.nargs)))
+        s[argid] .= "p" .* string.(argid) .* "::$sender_type"
+        if !allargs
             s = s[1:argid[end]]
             push!(s, "args...")
         end
@@ -56,85 +56,85 @@ function forward(send::Tuple{Type,Symbol}, recvs::Vector{T}, methods::Vector{Met
         m = string(method.module) * "."
         l *= ") = $m:(" * string(method.name) * ")("
         s = "p" .* string.(1:method.nargs-1)
-        if !useallargs
+        if !allargs
             s = s[1:argid[end]]
             push!(s, "args...")
         end
-        s[argid] .= "getfield(" .* s[argid] .* ", :$send_symb)"
+        s[argid] .= "getfield(" .* s[argid] .* ", :$sender_symb)"
         l *= join(s, ", ") * "; kw...)"
         l = join(split(l, "#"))
         return l
     end
 
-    @assert length(findall(isconcretetype.(recvs))) .<= 1 "Multiple concrete types in receivers array"
-
-    modu = Vector{Module}()
+    sender_type = string(parentmodule(sender[1])) * "." * string(nameof(sender[1]))
+    sender_symb = string(sender[2])
     code = Vector{String}()
-    send_type = string(parentmodule(send[1])) * "." * string(nameof(send[1]))
-    send_symb = string(send[2])
-
-    for method in methods
-        if method.name == :eval
-            @warn "Skipping `eval` method"
-            continue
+    
+    # Seacrh for receiver type in method arguments
+    foundat = Vector{Int}()
+    foundsender = false
+    for i in 2:method.nargs
+        argtype = fieldtype(method.sig, i)
+        if argtype != Any
+            @assert sender[1] != argtype
+            (typeintersect(receiver, argtype) != Union{})  &&  (push!(foundat, i-1))
         end
-        # Seacrh for receiver types in method arguments
-        foundat = Vector{Int}()
-        foundsender = false
-        for i in 2:method.nargs
-            argtype = fieldtype(method.sig, i)
-            if argtype != Any
-                if send[1] == argtype
-                    foundsender = true
-                    break
-                end
-                for recvtype in recvs
-                    tt = typeintersect(recvtype, argtype)
-                    if tt != Union{}
-                        push!(foundat, i-1)
-                        break
-                    end
-                end
-            end
-        end
-        (foundsender)  &&  (continue)  # Avoid redefining methods involving sender
-        if length(foundat) == 0
-            @warn "Skipping method since it doesn't involve any of the destination types"
-            display(method);  println();  continue
-        end
-
-        if length(foundat) > 1  # TODO: consider all possible combinations
-            for argid in foundat
-                l = fwd_method_as_string(send_type, send_symb, [argid], method, usetypes, useallargs)
-                push!(code, l)
-                push!(modu, method.module)
-            end
-        end
-        l = fwd_method_as_string(send_type, send_symb, foundat, method, usetypes, useallargs)
-        push!(code, l)
-        push!(modu, method.module)
     end
-    ii = unique(i->code[i], 1:length(code))
-    modu = modu[ii]
-    code = code[ii]
-    return (modu, code)
+    (length(foundat) == 0)  &&  (return code)
+    
+    for argid in foundat
+        push!(code, newmethod(sender_type, sender_symb, [argid], method, withtypes, allargs))
+    end
+    if length(foundat) > 1
+        push!(code, newmethod(sender_type, sender_symb, foundat, method, withtypes, allargs))
+        if length(foundat) >= 3
+            @warn "The following method accept the same argument three or more times.  Not all combinations will be automatically forwarded."
+            display(method)
+            println()
+        end
+    end
+    code = unique(code)
+    return "@eval " .* string(method.module) .* " " .* code
 end
 
 
-macro forward(send, recvs, ekws...)
+function forward(sender::Tuple{Type,Symbol}, receiver::Type, methods::Vector{Method}; kw...)
+    code = Vector{String}()
+    for m in methods
+        append!(code, forward(sender, receiver, m; kw...))
+    end
+    code = unique(code)
+    return code
+end
+
+function forward(sender::Tuple{Type,Symbol}, receivers::Vector{T}, methods; kw...) where T <: Type
+    code = Vector{String}()
+    for t in receivers
+        append!(code, forward(sender, t, methods; kw...))
+    end
+    code = unique(code)
+    return code
+end
+
+function forward(sender::Tuple{Type,Symbol}, receiver::Type; super=true, kw...)
+    tt = [receiver]
+    (super)  &&  (append!(tt, supertypes(receiver)))
+    return forward(sender, tt, methodswith(receiver, supertypes=super); kw...)
+end
+
+
+
+macro forward(sender, receiver, ekws...)
     kws = Vector{Pair{Symbol,Any}}()
     for kw in ekws
         push!(kws, Pair(kw.args[1], kw.args[2]))
     end
     out = quote
-        (m, code) = forward($send, $recvs; $kws...)
-        for i in 1:length(code)
-            line = code[i]
+        for line in forward($sender, $receiver; $kws...)
             try
-                m[i].eval(Meta.parse("eval(:($line))"))
+                eval(Meta.parse("$line"))
             catch err
                 println()
-                println("Module: $(m[i])")
                 println("$line")
                 @error err;
             end
@@ -142,6 +142,7 @@ macro forward(send, recvs, ekws...)
     end
     return esc(out)
 end
+
 
 
 macro inherit_fields(T)
