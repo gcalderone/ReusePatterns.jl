2fa1c297e1881ffede007c32a9a11a561222ea22
Giorgio Calderone <giorgio.calderone@gmail.com>
2019-01-11 12:23:39 +0100

    Added inheritablesubtypes method and runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,13 @@
+using ReusePatterns
+using Test
+
+
+supertypes(Int)
+supertypes(String)
+supertypes(Vector)
+supertypes(Vector{String})
+supertypes(Array)
+supertypes(UnionAll)
+supertypes(Union{Type,UnionAll,DataType})
+supertypes(Tuple{Int,Int})
+supertypes(Cartesian)
